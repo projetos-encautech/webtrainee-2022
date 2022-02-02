a9fb1be44d0f3ce12ae7712806b73f3d70dc135a
cosendeyluiz <cosendeyluiz@gmail.com>
2022-02-02 16:30:36 -0300

    add link

---
+++ teste-login/script.js
@@ -0,0 +1,93 @@
+INFOS = {
+
+    Dados:{
+        get: function (n) {
+
+			return JSON.parse(localStorage.getItem(n));		
+		},
+		
+		set: function (n, obj) {
+			
+			return localStorage.setItem(n, JSON.stringify(obj));	
+		}
+
+    },
+
+    Functions:{
+
+        Login:{
+
+            load: function () {
+				
+				var users = INFOS.Dados.get('usuarios');
+				
+				if(users != null && users.length > 0){
+					return true;					
+                }		
+				
+				var usuarios = [
+					 {"email": "emily", "senha": "bcf"},
+					 {"email": "emmanuel", "senha": "a123b"},
+					 {"email": "igor", "senha": "ig43"},
+					 {"email": "luiz", "senha": "143gg"},
+					 {"email": "rodolfo", "senha": "don0"},
+				];
+				
+				Infos.Dados.set('usuarios', usuarios);	
+            }, 
+
+            testaLogin: function(){
+                
+				var usuarios = INFOS.Dados.get('usuarios');
+				
+				var usuario = document.getElementById('email').value;
+				
+				usuario = usuario.toLowerCase();
+				
+				var senha = document.getElementById('senha').value;
+				
+				senha = senha.toLowerCase();
+			 
+				if(usuarios != null && usuarios.length > 0){
+					
+					var u = usuarios.find(a=> a.email == usuario && a.senha == senha);
+					
+					if(u != null){
+						
+						alert("PDados corretos para o usuario: " + u.email);
+						return true;                      
+					} 
+					alert("CDados incorretos, tente novamente.");
+					return false;
+				}              
+              
+                alert("JDados incorretos, tente novamente.");
+                return false;
+                },
+            voltaTelaLogin : function(){
+                    window.location = "telaLogin.html";    
+                }
+
+        },           				
+ 	
+		EsqueciSenha: {
+			RecuperaSenha: function (){
+				window.location = "telaRecuperaSenha.html";
+			},
+
+			EnviaSenhaRecuperacao: function(){
+                alert('Link de recuperação enviado' );
+			}	
+		},
+		
+		CadastroUsuario: {
+			
+			cadastroUsuario: function () {
+				// window.location("https://github.com/projetos-encautech/webtrainee-2022/blob/main/consultar-gado/index.html");				
+			}
+			
+		}
+		
+    }
+
+};
